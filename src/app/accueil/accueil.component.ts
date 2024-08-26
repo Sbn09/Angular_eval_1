6ab3fc5104a1dd0089246656b81c8302e4942e51
fbansept <bansept.franck@gmail.com>
2024-08-26 15:50:40 +0200

    suppression,  deplacement categore

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -35,12 +35,34 @@ export class AccueilComponent {
     },
     {
       nom: 'nul',
-      images: [],
+      images: [
+        'https://media.vogue.fr/photos/5e4bb48e9680cf00087e0e72/2:3/w_2560%2Cc_limit/82341124_2450116928574569_2617825020968777009_n.jpg',
+      ],
     },
   ];
 
   onClicAjouter() {
     this.categories[0].images.push(this.urlImage);
     this.urlImage = '';
+  }
+
+  onClicChangementCategorie(
+    indexCategorie: number,
+    indexImage: number,
+    moins: boolean,
+  ) {
+    //on récupère l'url de l'image cliquée
+    const urlImage = this.categories[indexCategorie].images[indexImage];
+
+    //on ajoute l'image dans la catégorie inférieure si moins = true, et supérieure si moins = false
+    this.categories[indexCategorie + (moins ? 1 : -1)].images.push(urlImage);
+
+    //on supprime l'image de la catégorie actuelle
+    this.categories[indexCategorie].images.splice(indexImage, 1);
+  }
+
+  onClicSupprime(indexCategorie: number, indexImage: number) {
+    //on supprime l'image de la catégorie actuelle
+    this.categories[indexCategorie].images.splice(indexImage, 1);
   }
 }
