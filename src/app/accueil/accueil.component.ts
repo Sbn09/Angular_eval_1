8992669cb5c7b842e0de5f991d8e49bfb6314107
fbansept <bansept.franck@gmail.com>
2024-08-26 13:47:32 +0200

    liste categories

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -8,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrl: './accueil.component.scss',
 })
 export class AccueilComponent {
-  titre: string = "Page d'accueil";
+  categories = [
+    {
+      nom: 'tres bien',
+      images: [
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoUbGxdooqKHGls6i0vJT0yXIIHN-PJtllBw&s',
+      ],
+    },
+    {
+      nom: 'bien',
+      images: [],
+    },
+    {
+      nom: 'moyen',
+      images: [],
+    },
+    {
+      nom: 'bof',
+      images: [],
+    },
+    {
+      nom: 'nul',
+      images: [],
+    },
+  ];
 }
